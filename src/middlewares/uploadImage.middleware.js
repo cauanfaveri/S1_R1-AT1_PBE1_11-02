8328324b10f9ccce7123300036966d812bc48506
cauanfaveri <cauanfaveri05@gmail.com>
2026-06-19 10:45:11 -0300

    daad

--- src/middlewares/uploadImage.middleware.js
+++ src/middlewares/uploadImage.middleware.js
@@ -4,6 +4,7 @@ const uploadImage = createMulter({
     folder: 'Images',
     allowedTypes: ['image/jpeg', 'image/png', 'image/jpg'],
     fileSize: 10 * 1024 * 1024 
-}).single('vinculoImagem'); // ✅ trocou 'image' por 'vinculoImagem'
+}).single('image');
+
 
 export default uploadImage;
